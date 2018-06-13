1af658f3329eb21beff18c31f84e9290766f1d14
Jacek Wegorkiewicz <Jacek_Wegorkiewicz@epam.com>
2018-06-13 16:15:20 +0200

    commit 3

---
+++ src/main/java/TestClass2.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jacek_Wegorkiewicz on 07.06.2018.
+ */
+public class TestClass2 {
+    public static void main(String[] args) {
+        System.out.println("Hello CI!");
+    }
+}
