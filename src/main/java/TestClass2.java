eb3d72a85c3bc7e7350f43f3b2e32b082dc84707
Jacek Wegorkiewicz <Jacek_Wegorkiewicz@epam.com>
2018-06-25 13:31:12 +0200

    jenkins check

--- src/main/java/TestClass2.java
+++ src/main/java/TestClass2.java
@@ -3,6 +3,6 @@
  */
 public class TestClass2 {
     public static void main(String[] args) {
-        System.out.println("Hello CI!");
+        System.out.println("Hello CI! I'm modified");
     }
 }
